25cfed4081f025d9463513a696d56de165c63658
TheoManelphe <43880934+TheoManelphe@users.noreply.github.com>
2019-05-02 19:01:08 +0200

    Gardens terminé

--- src/main/java/fr/umontpellier/iut/dominion/cards/base/Gardens.java
+++ src/main/java/fr/umontpellier/iut/dominion/cards/base/Gardens.java
@@ -1,5 +1,7 @@
 package fr.umontpellier.iut.dominion.cards.base;
 
+import fr.umontpellier.iut.dominion.ListOfCards;
+import fr.umontpellier.iut.dominion.Player;
 import fr.umontpellier.iut.dominion.cards.Card;
 
 /**
@@ -10,5 +12,27 @@ import fr.umontpellier.iut.dominion.cards.Card;
 public class Gardens extends Card {
     public Gardens() {
         super("Gardens", 4);
+    }
+
+
+
+
+
+
+    @Override
+
+    public int getVictoryValue(Player p) {
+        int disaines = 0;
+        int unite = 0;
+
+        for( Card c : p.getAllCards()){
+            unite ++;
+
+            if (unite == 10){
+                unite = 0;
+                disaines ++;
+            }
+        }
+        return disaines;
     }
 }
