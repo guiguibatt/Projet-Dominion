4941d9778f23bad0c63d4729da00f88a2be3adce
TheoManelphe <43880934+TheoManelphe@users.noreply.github.com>
2019-05-03 14:58:10 +0200

    Chapel terminé!

--- src/main/java/fr/umontpellier/iut/dominion/cards/base/Chapel.java
+++ src/main/java/fr/umontpellier/iut/dominion/cards/base/Chapel.java
@@ -1,6 +1,11 @@
 package fr.umontpellier.iut.dominion.cards.base;
 
+import fr.umontpellier.iut.dominion.ListOfCards;
+import fr.umontpellier.iut.dominion.Player;
 import fr.umontpellier.iut.dominion.cards.Card;
+
+import java.util.Arrays;
+import java.util.List;
 
 /**
  * Carte Chapelle (Chapel)
@@ -11,4 +16,22 @@ public class Chapel extends Card {
     public Chapel() {
         super("Chapel", 2);
     }
-}
+
+    @Override
+    public void play(Player p) {
+            ListOfCards CardInHand = p.getCardsInHand();
+            List<String> choices = Arrays.asList("yes","no");
+            int i = 0;
+            String chooseAgain = "yes";
+
+            while (chooseAgain == "yes" && i < 4) {
+
+            p.chooseCard("Choisissez une carte à écarter",CardInHand,false );
+            chooseAgain =  p.chooseOption("Voulez vous continuer à jeter des cartes?"
+                    ,choices,false);
+
+            i++;
+
+            }
+        }
+    }
