7ad1c384b117ab945ed111852b24bce73d0a1eb0
TheoManelphe <43880934+TheoManelphe@users.noreply.github.com>
2019-05-03 15:17:40 +0200

    Cellar

--- src/main/java/fr/umontpellier/iut/dominion/cards/base/Cellar.java
+++ src/main/java/fr/umontpellier/iut/dominion/cards/base/Cellar.java
@@ -1,6 +1,11 @@
 package fr.umontpellier.iut.dominion.cards.base;
 
+import fr.umontpellier.iut.dominion.ListOfCards;
+import fr.umontpellier.iut.dominion.Player;
 import fr.umontpellier.iut.dominion.cards.Card;
+
+import java.util.Arrays;
+import java.util.List;
 
 /**
  * Carte Cave (Cellar)
@@ -13,4 +18,29 @@ public class Cellar extends Card {
     public Cellar() {
         super("Cellar", 2);
     }
+
+    @Override
+    public void play(Player p) {
+        ListOfCards CardInHand = p.getCardsInHand();
+        List<String> choices = Arrays.asList("yes","no");
+        int i = 0;
+        String chooseAgain = "yes";
+
+        p.incrementActions(1);
+
+        while (chooseAgain == "yes"){
+
+            p.chooseCard("Choisissez une carte à écarter",CardInHand,false );
+            chooseAgain =  p.chooseOption("Voulez vous continuer à jeter des cartes?"
+                    ,choices,false);
+
+            i++;
+
+        }
+
+        while (i != 0){
+        p.drawToHand();
+        i--;
+    }
+}
 }
