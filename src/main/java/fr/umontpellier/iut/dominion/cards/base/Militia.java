7d411c19b3adfd73526a9e93d96658ff09ce69db
TheoManelphe <43880934+TheoManelphe@users.noreply.github.com>
2019-05-08 17:55:15 +0200

    militia terminé

--- src/main/java/fr/umontpellier/iut/dominion/cards/base/Militia.java
+++ src/main/java/fr/umontpellier/iut/dominion/cards/base/Militia.java
@@ -1,6 +1,11 @@
 package fr.umontpellier.iut.dominion.cards.base;
 
+import fr.umontpellier.iut.dominion.ListOfCards;
+import fr.umontpellier.iut.dominion.Player;
 import fr.umontpellier.iut.dominion.cards.Card;
+
+import java.util.Arrays;
+import java.util.List;
 
 /**
  * Carte Milice (Militia)
@@ -11,5 +16,23 @@ import fr.umontpellier.iut.dominion.cards.Card;
 public class Militia extends Card {
     public Militia() {
         super("Militia", 4);
+    }
+
+    public void play(Player p) {
+
+        p.incrementMoney(2);
+
+
+        List<Player> opponents;
+        opponents = p.getOtherPlayers();
+        ListOfCards CardInHand = p.getCardsInHand();
+
+        for (Player op : opponents) {
+            while (op.getCardsInHand().size() > 3) {
+
+                p.chooseCard("Choisissez une carte à écarter", CardInHand, false);
+
+            }
+        }
     }
 }
