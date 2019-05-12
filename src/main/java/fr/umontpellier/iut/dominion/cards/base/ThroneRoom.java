e7e0e9c2412f77891662e43ffedcf8b02a312657
reda <reda.regam@etu.umontpellier.fr>
2019-05-12 23:23:21 +0200

    Throne Room finie, test a faire

--- src/main/java/fr/umontpellier/iut/dominion/cards/base/ThroneRoom.java
+++ src/main/java/fr/umontpellier/iut/dominion/cards/base/ThroneRoom.java
@@ -1,6 +1,7 @@
 package fr.umontpellier.iut.dominion.cards.base;
 
 import fr.umontpellier.iut.dominion.cards.Card;
+import fr.umontpellier.iut.dominion.Player;
 
 /**
  * Carte Salle du trône (Throne Room)
@@ -15,6 +16,16 @@ public class ThroneRoom extends Card {
 
     public ThroneRoom(String name, int cost) {
         super(name, cost);
+    }
+
+    public void play(Player p) {
+        String cardName = p.chooseCard("Choisissez 1 carte Action de votre main a jouer 2 fois", p.getCardsInHand(), true);
+        if (!cardName.equals("")) {
+            p.playCard(cardName);
+            p.getHand().add(p.getInPlay().remove(cardName));
+            p.playCard(cardName);
+        }
+
     }
 
 }
