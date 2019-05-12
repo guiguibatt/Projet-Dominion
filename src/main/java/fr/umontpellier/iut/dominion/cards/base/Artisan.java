f6992f9bab14aaf63a1cbb62182d131a3613c12e
TheoManelphe <43880934+TheoManelphe@users.noreply.github.com>
2019-05-12 17:42:43 +0200

    artisan validé

--- src/main/java/fr/umontpellier/iut/dominion/cards/base/Artisan.java
+++ src/main/java/fr/umontpellier/iut/dominion/cards/base/Artisan.java
@@ -1,5 +1,7 @@
 package fr.umontpellier.iut.dominion.cards.base;
 
+import fr.umontpellier.iut.dominion.ListOfCards;
+import fr.umontpellier.iut.dominion.Player;
 import fr.umontpellier.iut.dominion.cards.Card;
 
 /**
@@ -11,5 +13,36 @@ import fr.umontpellier.iut.dominion.cards.Card;
 public class Artisan extends Card {
     public Artisan() {
         super("Artisan", 6);
+    }
+
+    @Override
+    public void play(Player p) {
+
+        ListOfCards cardsInSupply = p.getCardsInSupply();
+
+        boolean n = false;
+        while (!n) {
+            String choice = p.chooseCard("Choisissez une carte à ajouter à votre main", cardsInSupply, false);
+
+            Card cardFound = p.getGame().removeFromSupply(choice);
+            if (cardFound != null && cardFound.getCost() < 6) {
+                p.gainFromSupply(cardFound.getName());
+                p.addToHand(cardFound);
+                p.removeFromDiscard(cardFound.getName());
+                n = true;
+            }
+        }
+
+        ListOfCards cardsInHand = p.getCardsInHand();
+
+        String choice2 = p.chooseCard("Choisissez une carte à ajouter sur votre deck", cardsInHand, false);
+
+        for (Card c : p.getCardsInHand()) {
+            if (choice2.equals(c.getName())) {
+                p.addToDraw(c);
+                p.removeFromHand(choice2);
+            }
+
+        }
     }
 }
