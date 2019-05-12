7112246d53e20ee7df133795ad0f2609e2ffa29e
TheoManelphe <43880934+TheoManelphe@users.noreply.github.com>
2019-05-12 17:11:53 +0200

    sentry avec probleme tres etrange

--- src/main/java/fr/umontpellier/iut/dominion/cards/base/Sentry.java
+++ src/main/java/fr/umontpellier/iut/dominion/cards/base/Sentry.java
@@ -1,5 +1,7 @@
 package fr.umontpellier.iut.dominion.cards.base;
 
+import fr.umontpellier.iut.dominion.ListOfCards;
+import fr.umontpellier.iut.dominion.Player;
 import fr.umontpellier.iut.dominion.cards.Card;
 
 /**
@@ -13,5 +15,49 @@ import fr.umontpellier.iut.dominion.cards.Card;
 public class Sentry extends Card {
     public Sentry() {
         super("Sentry", 5);
+    }
+
+    @Override
+    public void play(Player p) {
+
+        p.drawToHand();
+        p.incrementActions(1);
+
+        ListOfCards twoFromDeck = new ListOfCards();
+        twoFromDeck.add(p.drawCard());
+        twoFromDeck.add(p.drawCard());
+
+
+        String choiceTrash = "texte-non-vide";
+        String choiceDiscard = "texte-non-vide";
+        int i = 0;
+
+        while (!choiceTrash.equals("") || i < 2) {
+            choiceTrash = p.chooseCard("Choisissez une carte à écarter", twoFromDeck, true);
+            i++;
+
+            for (Card c : twoFromDeck) {
+                if (choiceTrash.equals(c.getName())) {
+                    twoFromDeck.remove(c.getName());
+                }
+            }
+        }
+
+
+        while (!choiceDiscard.equals("") || i < 3) {
+            choiceDiscard = p.chooseCard("Choisissez une carte à défausser", twoFromDeck, false);
+            i++;
+
+            for (Card c : twoFromDeck) {
+                if (choiceDiscard.equals(c.getName())) {
+                    p.addToDraw(c);
+                    twoFromDeck.remove(c);
+
+                }
+
+            }
+
+
+        }
     }
 }
