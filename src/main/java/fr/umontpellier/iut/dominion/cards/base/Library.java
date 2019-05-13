6d5fdcfe4e68e5f8463c5dd52ea952b8ed34f9a0
reda <reda.regam@etu.umontpellier.fr>
2019-05-13 02:14:54 +0200

    Library casi finie

--- src/main/java/fr/umontpellier/iut/dominion/cards/base/Library.java
+++ src/main/java/fr/umontpellier/iut/dominion/cards/base/Library.java
@@ -1,7 +1,11 @@
 package fr.umontpellier.iut.dominion.cards.base;
 
+import fr.umontpellier.iut.dominion.ListOfCards;
 import fr.umontpellier.iut.dominion.cards.Card;
-
+import fr.umontpellier.iut.dominion.Player;
+import fr.umontpellier.iut.dominion.CardType;
+import java.util.ArrayList;
+import java.util.Arrays;
 /**
  * Carte Bibliothèque (Library)
  *
@@ -11,5 +15,23 @@ import fr.umontpellier.iut.dominion.cards.Card;
 public class Library extends Card {
     public Library() {
         super("Library", 5);
+    }
+
+    public void play(Player p) {
+        ListOfCards carteDeCote = new ListOfCards();
+
+        while(p.getHand().size() < 7) {
+            Card cartePioche = p.drawToHand();
+            if (cartePioche != null && cartePioche.getTypes().contains(CardType.Action)) {
+
+                String veutMettreDeCote = p.choose("Do you want to put it in the discard instead of your hand?", new ArrayList(Arrays.asList("y", "n")), false, false);
+                if (veutMettreDeCote.equalsIgnoreCase("y")) {
+                    carteDeCote.add(p.getHand().remove(cartePioche.getName()));
+
+                }
+            }
+        }
+
+        p.getDiscard().addAll(carteDeCote);
     }
 }
