33c6bdd1eddb5d9cad4bf3307d414e501bb8bc15
redaregam <reda.regam@etu.umontpellier.fr>
2019-05-13 16:08:50 +0200

    ActionCard et ReactionCard faite /et package type Créé

---
+++ src/main/java/fr/umontpellier/iut/dominion/cards/common/type/ActionCard.java
@@ -0,0 +1,18 @@
+package fr.umontpellier.iut.dominion.cards.common.type;
+import fr.umontpellier.iut.dominion.cards.Card;
+import fr.umontpellier.iut.dominion.CardType;
+
+import java.util.List;
+
+public abstract class ActionCard extends Card {
+    public ActionCard(String name, int cost) {
+        super(name, cost);
+    }
+
+    public List<CardType> getTypes() {
+        List<CardType> types = super.getTypes();
+        types.add(CardType.Action);
+        return types;
+    }
+}
+
---
+++ src/main/java/fr/umontpellier/iut/dominion/cards/common/type/ReactionCard.java
@@ -0,0 +1,16 @@
+package fr.umontpellier.iut.dominion.cards.common.type;
+
+import fr.umontpellier.iut.dominion.CardType;
+
+import java.util.List;
+public abstract class ReactionCard extends ActionCard {
+    public ReactionCard(String name, int cost) {
+        super(name, cost);
+    }
+
+    public List<CardType> getTypes() {
+        List<CardType> types = super.getTypes();
+        types.add(CardType.Reaction);
+        return types;
+    }
+}
