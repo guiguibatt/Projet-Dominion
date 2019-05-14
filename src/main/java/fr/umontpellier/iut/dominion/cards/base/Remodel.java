e55a9f20ed858f7fd0e7bf3acea2a2d1fd665060
TheoManelphe <43880934+TheoManelphe@users.noreply.github.com>
2019-05-14 17:27:43 +0200

    remodel test passé

--- src/main/java/fr/umontpellier/iut/dominion/cards/base/Remodel.java
+++ src/main/java/fr/umontpellier/iut/dominion/cards/base/Remodel.java
@@ -1,5 +1,7 @@
 package fr.umontpellier.iut.dominion.cards.base;
 
+import fr.umontpellier.iut.dominion.ListOfCards;
+import fr.umontpellier.iut.dominion.Player;
 import fr.umontpellier.iut.dominion.cards.Card;
 
 /**
@@ -11,5 +13,36 @@ import fr.umontpellier.iut.dominion.cards.Card;
 public class Remodel extends Card {
     public Remodel() {
         super("Remodel", 4);
+    }
+
+    @Override
+    public void play(Player p) {
+        ListOfCards cardsInHand = new ListOfCards();
+        cardsInHand = p.getCardsInHand();
+        int cout = 0;
+
+        String choice = p.chooseCard("Choisissez une carte à écarter", cardsInHand, false);
+
+        for (Card c : new ListOfCards( p.getCardsInHand()) ){
+            if (choice.equals(c.getName())) {
+                cout = c.getCost() +3; // le plus trois compense le fait que ce soit un < et pas un <= dans la verification du coût
+
+                p.removeFromHand(choice);
+
+            }
+        }
+
+        ListOfCards cardsInSupply = p.getCardsInSupply();
+
+        boolean n = false;
+        while(!n){
+            String choice2 = p.chooseCard("Choisissez une carte coutant 2 de plus que celle jetée à ajouter à votre pioche", cardsInSupply ,false );
+
+            Card cardFound = p.getGame().removeFromSupply(choice2);
+            if(cardFound != null && cardFound.getCost() < cout ) {
+                p.gain(cardFound);
+                n = true;
+            }
+        }
     }
 }
